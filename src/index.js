4c0b0ebfcf163de0163ca8af458faaad910b6915
tonitdiep <adiep123@gmail.com>
2021-07-25 21:10:33 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -6,14 +6,17 @@ import About from './About'
 import Login from './Login'
 import Navbar from './Navbar'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import {Redirect } from 'react-router-dom';
 ReactDOM.render((
   <Router>
     <div>
       <Navbar />
       <Route exact path="/" component={Home} />
       <Route exact path="/about" component={About} />
-      <Route exact path="/login" component={Login} />
+      <Route exact path="/login" component={Login} 
+      
+      />
+      <Redirect   from="/login" to="/about" message="redirecting"/>
     </div>
   </Router>),
   document.getElementById('root')
